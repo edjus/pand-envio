0f6551cd0e30b91dfed412eeb6629e7740ce1bf3
edjus <ejusto@fi.uba.ar>
2020-08-19 00:48:17 -0300

    test: Item admite o no descuento según el producto

--- server/src/test/groovy/pandenvio/ItemSpec.groovy
+++ server/src/test/groovy/pandenvio/ItemSpec.groovy
@@ -12,8 +12,23 @@ class ItemSpec extends Specification implements DomainUnitTest<Item> {
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == true
+    void "test Item admite descuento si el producto lo admite"() {
+        given:
+            Item item = new Item(new Plato(), 1)
+            CuponDescuento cupon = new CuponDescuentoPorcentual(activo: true, fecha: new Date())
+        when:
+            Boolean admite = item.admiteA(cupon)
+        then:
+            admite.equals(true)
+    }
+
+    void "test Item no admite descuento si el producto no lo admite"() {
+        given:
+            Item item = new Item(new Menu(), 1)
+            CuponDescuento cupon = new CuponDescuentoPorcentual(activo: true, fecha: new Date())
+        when:
+            Boolean admite = item.admiteA(cupon)
+        then:
+            admite.equals(false)
     }
 }
