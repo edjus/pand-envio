89b992f8bd8867f06ea8e731b534a5e8599530ff
edjus <ejusto@fi.uba.ar>
2020-08-19 01:02:29 -0300

    test: calcular precuo item

--- server/src/test/groovy/pandenvio/ItemSpec.groovy
+++ server/src/test/groovy/pandenvio/ItemSpec.groovy
@@ -6,7 +6,14 @@ import spock.lang.Specification
 
 class ItemSpec extends Specification implements DomainUnitTest<Item> {
 
+    Plato plato
+    Menu menu
+    CuponDescuento cupon
+    
     def setup() {
+        plato = new Plato(nombre: 'Alto Guiso', precio: 15, categoria: CategoriaPlato.PLATO)
+        menu = new Menu(nombre: 'Viernes', precio: 300)
+        cupon = new CuponDescuentoPorcentual(activo: true, fecha: new Date())
     }
 
     def cleanup() {
@@ -14,8 +21,7 @@ class ItemSpec extends Specification implements DomainUnitTest<Item> {
 
     void "test Item admite descuento si el producto lo admite"() {
         given:
-            Item item = new Item(new Plato(), 1)
-            CuponDescuento cupon = new CuponDescuentoPorcentual(activo: true, fecha: new Date())
+            Item item = new Item(plato, 1)
         when:
             Boolean admite = item.admiteA(cupon)
         then:
@@ -24,11 +30,21 @@ class ItemSpec extends Specification implements DomainUnitTest<Item> {
 
     void "test Item no admite descuento si el producto no lo admite"() {
         given:
-            Item item = new Item(new Menu(), 1)
-            CuponDescuento cupon = new CuponDescuentoPorcentual(activo: true, fecha: new Date())
+            Item item = new Item(menu, 1)
         when:
             Boolean admite = item.admiteA(cupon)
         then:
             admite.equals(false)
+    }
+
+    void "test el precio del Item es igual al precio del producto por la cantidad"() {
+        given:
+            BigDecimal precioPlato = 15
+            Integer cantidad = 2
+            Item item = new Item(new Plato(precio: precioPlato), cantidad)
+        when:
+            BigDecimal precioItem = item.calcularPrecio()
+        then:
+            precioItem == precioPlato * cantidad
     }
 }
