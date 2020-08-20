6e4376ede0241e49a3c946ed11156e611dc12b60
edjus <ejusto@fi.uba.ar>
2020-08-19 23:58:13 -0300

    test: pedido tiene que tener un cliente

--- server/src/test/groovy/pandenvio/PedidoSpec.groovy
+++ server/src/test/groovy/pandenvio/PedidoSpec.groovy
@@ -15,7 +15,15 @@ class PedidoSpec extends Specification implements DomainUnitTest<Pedido> {
         when:
             Pedido pedido = new Pedido(new Cliente())
         then:
+            pedido.validate()
             pedido.fecha.toString() == (new Date()).toString()
             pedido.cuponDeDescuento.class == CuponDescuentoNulo
+    }
+
+    void "test cliente de un Pedido no puede ser null"() {
+        when:
+            Pedido pedido = new Pedido()
+        then:
+            !pedido.validate()
     }
 }
