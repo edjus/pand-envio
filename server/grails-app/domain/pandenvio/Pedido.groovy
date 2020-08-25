6b8f894c7e0b49c9aaebe3c4365f4df6800b5214
edjus <ejusto@fi.uba.ar>
2020-08-24 22:48:54 -0300

    test: Pedido se guarda bien y se persiste el nombre del estado

--- server/grails-app/domain/pandenvio/Pedido.groovy
+++ server/grails-app/domain/pandenvio/Pedido.groovy
@@ -2,10 +2,13 @@ package pandenvio
 
 class Pedido {
 
+    static transients = [ "estado" ] // No persiste el atributo estado
+
     Date fecha
     CuponDescuento cuponDeDescuento
     Cliente cliente
     EstadoPedido estado
+    String nombreEstado
     List<Item> items = []
 
     static constraints = {
@@ -16,7 +19,7 @@ class Pedido {
         this.fecha = new Date()
         this.cliente = cliente
         this.cuponDeDescuento = new CuponDescuentoNulo(fecha: fecha, activo: true, codigo: 'NULO')
-        this.estado = new EstadoRecibido()
+        this.setEstado(new EstadoRecibido())
     }
 
     void agregar(Producto producto, Integer cantidad){
@@ -28,5 +31,11 @@ class Pedido {
         Boolean aplicaDescuento = items.inject(true) {resultado , item -> resultado && item.admiteA(cuponDeDescuento)}
 
         aplicaDescuento ? cuponDeDescuento.aplicarDescuento(precioTotalParcial) : precioTotalParcial
+    }
+
+    // TODO: ver como mejorar ésto, es una asignación manual del estado al cargar el pedido
+    void setEstado(EstadoPedido nuevoEstado){
+        this.estado = nuevoEstado
+        this.nombreEstado = nuevoEstado.nombre
     }
 }
--- server/src/integration-test/groovy/pand/envio/PersitenciaSpec.groovy
+++ server/src/integration-test/groovy/pand/envio/PersitenciaSpec.groovy
@@ -4,10 +4,16 @@ package pand.envio
 import grails.testing.mixin.integration.Integration
 import grails.gorm.transactions.Rollback
 import pandenvio.CategoriaPlato
+import pandenvio.Cliente
+import pandenvio.CuponDescuento
+import pandenvio.CuponDescuentoPorcentual
 import pandenvio.Item
 import pandenvio.Menu
+import pandenvio.Pedido
 import pandenvio.Plato
+import pandenvio.Producto
 import pandenvio.Restaurant
+import pandenvio.Ubicacion
 import spock.lang.Specification
 
 @Integration
@@ -33,5 +39,34 @@ class PersitenciaSpec extends Specification {
             def itemGuardado = Item.findById(item.id)
             itemGuardado ==  item
             itemGuardado.producto == menuConPLatos
+    }
+
+
+    void "test pedido se guarda bien"() {
+        given:
+            Restaurant restaurante = new Restaurant(nombre: 'La otra esquina').save(failOnError: true)
+            Ubicacion unaCasa = new Ubicacion(calle:'Av. Siempre viva', altura: 1234).save(failOnError: true)
+            Cliente cliente = new Cliente(nombre: 'Moni', apellido: 'Argento',  mail: 'moni.argento@gmail.com', ubicacion: unaCasa, telefono: '11-5555-4433').save()
+            Producto menu = new Menu(nombre: 'Viernes', precio: 400, restaurant: restaurante)
+                    .addToPlatos(new Plato(nombre:'Milanesa con pure', precio: 350, categoria: CategoriaPlato.PLATO, restaurant:  restaurante))
+                    .save(failOnError: true)
+            Producto plato = new Plato(nombre: 'Alto Guiso', precio: 200, categoria: CategoriaPlato.PLATO, restaurant: restaurante)
+                        .save(failOnError: true)
+            CuponDescuento cupon = new CuponDescuentoPorcentual(activo: true, porcentaje: 10, codigo: 'ABC', fecha: new Date())
+                    .save(failOnError: true)
+
+            Pedido pedido = new Pedido(cliente)
+            pedido.agregar(plato, 2)
+            pedido.agregar(menu, 2)
+            pedido.cuponDeDescuento = cupon
+        when:
+            pedido.save(failOnError: true)
+        then:
+            Pedido.count == 1
+            def pedidoGuardado = Pedido.findById(pedido.id)
+            pedidoGuardado == pedido
+            pedidoGuardado.cliente == cliente
+            pedidoGuardado.nombreEstado == 'recibido'
+            pedidoGuardado.cuponDeDescuento == cupon
     }
 }
