e58c67689b44e76683d9475cb047dc80c27d1e00
edjus <ejusto@fi.uba.ar>
2020-08-24 21:47:48 -0300

    test: se agrega test persistencia de restaurante

--- server/src/test/groovy/pandenvio/RestaurantSpec.groovy
+++ server/src/test/groovy/pandenvio/RestaurantSpec.groovy
@@ -4,4 +4,12 @@ import grails.testing.gorm.DomainUnitTest
 import spock.lang.Specification
 
 class RestaurantSpec extends Specification implements DomainUnitTest<Restaurant> {
+    void "Restaurante se guarda correctamente"() {
+        when:
+        Restaurant restaurante = new Restaurant(nombre: 'La esquina')
+        restaurante.save()
+        then:
+        Restaurant.count == 1
+        restaurante == Restaurant.findById(restaurante.id)
+    }
 }
