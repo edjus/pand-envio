47ab60f98bf7a19e7493c9c60803b1c8f0b5c0f0
edjus <ejusto@fi.uba.ar>
2020-08-30 23:03:07 -0300

    feat: post crear pedido sin usar servicio

--- server/grails-app/controllers/pandenvio/PedidoController.groovy
+++ server/grails-app/controllers/pandenvio/PedidoController.groovy
@@ -6,6 +6,7 @@ import grails.converters.*
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST
 import static org.springframework.http.HttpStatus.CREATED
+import static org.springframework.http.HttpStatus.OK
 
 @Transactional(readOnly = true)
 class PedidoController {
@@ -22,6 +23,33 @@ class PedidoController {
         }
         else {
             return [pedido: pedido]
+        }
+    }
+
+    @Transactional
+    def save() {
+
+        // TODO: Crear PedidoService que reciba Long clienteId y String modalidad y usarlo para crear pedido
+        Cliente cliente = Cliente.findById(request.JSON.cliente_id)
+        ModalidadEntrega modalidadEntrega = obtenerModadalidad(request.JSON.modalidad)
+        if (!cliente || !modalidadEntrega){
+            // TODO: Ver como enviar mensaje de error
+            respond 'Parámetros inválidos', status: BAD_REQUEST
+        } else {
+            Pedido pedido= new Pedido(cliente, modalidadEntrega).save(failOnError: true)
+            respond([pedido: pedido], status: OK)
+        }
+    }
+
+
+    // TODO: Crear un ModalidadService que dado un string devuelva la modalidad
+    private ModalidadEntrega obtenerModadalidad(String modalidad){
+        if (modalidad == 'para_llevar'){
+            return  new ModalidadParaLlevar();
+        } else if (modalidad == 'para_retirar'){
+            return  new  ModalidadParaRetirar()
+        } else{
+            return null
         }
     }
 }
--- server/grails-app/domain/pandenvio/Menu.groovy
+++ server/grails-app/domain/pandenvio/Menu.groovy
@@ -2,6 +2,7 @@ package pandenvio
 
 class Menu extends Producto {
 
+    // TODO: Ver por qué no se guarda la relación en la base de datos
     static hasMany = [platos: Plato]
 
     boolean admiteA(CuponDescuento descuento) {
