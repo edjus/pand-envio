7a5b97899df5fe5238763b1facb78a38d89b977d
Pablo Rodriguez Massuh <pablomassuh@gmail.com>
2020-09-02 19:30:02 -0300

    Carga de Restaurants y botones de acciones

---
+++ client/src/components/table/FilaRestaurant.vue
@@ -0,0 +1,23 @@
+<template>
+  <tr>
+    <td>{{ item.id }}</td>
+    <td>{{ item.nombre }}</td>
+    <td>{{ item.direccion }}</td>
+    <td>
+      <button class="btn btn-primary"><i class="fas fa-edit"></i> Editar</button>
+      <button class="btn btn-danger"><i class="fas fa-trash-alt"></i> Eliminar</button>
+
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "FilaRestaurant",
+  props: ['item']
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/components/table/TablaRestaurant.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="table-responsive">
+    <table class="table table-striped table-sm">
+      <thead>
+      <tr>
+        <th>Id</th>
+        <th>Nombre</th>
+        <th>Direccion</th>
+        <th>Acciones</th>
+      </tr>
+      </thead>
+      <tbody>
+      <fila-restaurant v-for="restaurante in restaurantes"
+                 :item="restaurante" :key="restaurante.id"></fila-restaurant>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import FilaRestaurant from "./FilaRestaurant";
+export default {
+  name: "TablaRestaurant",
+  components: {FilaRestaurant},
+  props: ['restaurantes']
+}
+</script>
+
+<style scoped>
+
+</style>
