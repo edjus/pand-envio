7cca1be8c64c9dd1ea56ea6cdaae9a4663c1afa5
edjus <ejusto@fi.uba.ar>
2020-09-05 20:33:26 -0300

    chore: elimino prints

--- server/grails-app/domain/pandenvio/AsignadorRepartidor.groovy
+++ server/grails-app/domain/pandenvio/AsignadorRepartidor.groovy
@@ -9,13 +9,11 @@ class AsignadorRepartidor {
     }
 
     Repartidor obtenerRepartidor(Restaurant restaurant){
-        // TODO: Agregar lógica de repartidores disponibles
         def repartidores = Repartidor.findAllWhere(disponible: true, restaurant: restaurant)
         if (repartidores.empty){
             return null
         }
-         log.info("REPARTIDORES HABILATOS: ${repartidores.count}")
-        println("REPARTIDORES HABILATOS: ${repartidores.count}")
+
         repartidores.first()
     }
 }
