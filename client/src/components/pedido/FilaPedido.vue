2631c06014459cb3d492ab96cec5ae992c7493f5
Pablo Rodriguez Massuh <pablomassuh@gmail.com>
2020-09-17 20:54:24 -0300

    Vistas de Pedidos (vista admin)

---
+++ client/src/components/pedido/FilaPedido.vue
@@ -0,0 +1,31 @@
+<template>
+  <tr>
+    <td>{{ item.id }}</td>
+    <td>{{ item.fecha }}</td>
+    <td>{{item.cliente.nombre }}</td>
+    <td>{{ item.estado}}</td>
+    <td>{{ item.modalidad}}</td>
+    <td>{{ '$' + item.precio}}</td>
+    <td>{{ item.restaurant.nombre }}</td>
+    <td>
+      <button class='btn btn-primary' v-on:click="avanzar"><i class='fas fa-arrow-circle-right'></i> Avanzar estado</button>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'FilaPedido',
+  props: ['item'],
+  methods: {
+    avanzar: function () {
+      this.$emit('avanzarPedido', this.item)
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
