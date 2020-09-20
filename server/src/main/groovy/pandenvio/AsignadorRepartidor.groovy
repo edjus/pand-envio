61bfb9d33ab538a2d7c1a7ffd977c83ecde71345
afegonzalez <afgonzalez@fi.uba.ar>
2020-09-20 17:01:47 -0300

    fix test

--- server/src/main/groovy/pandenvio/AsignadorRepartidor.groovy
+++ server/src/main/groovy/pandenvio/AsignadorRepartidor.groovy
@@ -6,7 +6,9 @@ class AsignadorRepartidor {
     void asignarPara(Pedido pedido){
         Repartidor repartidor = obtenerRepartidor(pedido.restaurant)
         pedido.asignarA(repartidor)
-        repartidor.agregarPedido(pedido)
+        if(pedido != null){
+            repartidor.agregarPedido(pedido)
+            }
     }
 
     Repartidor obtenerRepartidor(Restaurant restaurant){
