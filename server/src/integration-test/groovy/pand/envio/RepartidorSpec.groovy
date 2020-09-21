5973d1603fde05e75ac67249b72e75ccb233d6d8
afegonzalez <afgonzalez@fi.uba.ar>
2020-09-20 21:41:34 -0300

    Subo test de integracion de repartidor

--- server/src/integration-test/groovy/pand/envio/RepartidorSpec.groovy
+++ server/src/integration-test/groovy/pand/envio/RepartidorSpec.groovy
@@ -65,6 +65,47 @@ class RepartidorSpec extends Specification {
                 (repartidor.listaDePedidos).size() == 1
     }
 
+        void "test repartidor tiene dos pedidos"() {
+        given:
+        Restaurant restaurante = new Restaurant(nombre: 'La otra esquina').save(failOnError: true)
+        Repartidor repartidor = new  Repartidor("Juan", "9798797", restaurante).save(failOnError: true)
+        Ubicacion unaCasa = new Ubicacion(calle:'Av. Siempre viva', altura: 1234).save(failOnError: true)
+        Cliente cliente = new Cliente(nombre: 'Moni', apellido: 'Argento',  mail: 'moni.argento@gmail.com', ubicacion: unaCasa, telefono: '11-5555-4433')
+                .save()
+        Producto plato = new Plato(nombre: 'Alto Guiso', precio: 200, categoria: CategoriaPlato.PLATO, restaurant: restaurante)
+                .save(failOnError: true)
+        ModalidadEntrega modalidadEntrega = new ModalidadParaLlevar()
+                .save(failOnError: true)
+        EstadoPedido estado = new EstadoListo().save(failOnError: true)
+        Pedido pedido = new Pedido(cliente, modalidadEntrega, restaurante)
+        pedido.agregar(plato, 2)
+        pedido.estado = estado
+        pedido.save(failOnError: true)
+        pedido.siguienteEstado()
+        pedido.siguienteEstado()
+        pedido.save(failOnError: true)
+        modalidadEntrega.save(failOnError: true)
+        pedido.save(failOnError: true)
+
+        ModalidadEntrega modalidadEntrega2 = new ModalidadParaLlevar()
+                .save(failOnError: true)
+
+        Pedido pedido2 = new Pedido(cliente, modalidadEntrega2, restaurante)
+        pedido2.agregar(plato, 2)
+        pedido2.estado = estado
+        pedido2.save(failOnError: true)
+        pedido2.siguienteEstado()
+        pedido2.siguienteEstado()
+        pedido2.save(failOnError: true)
+
+        modalidadEntrega.save(failOnError: true)
+        
+        when:
+                pedido.save(failOnError: true)
+        then:
+                (repartidor.listaDePedidos).size() == 2
+    }
+
     void "test repartidor tiene un pedido entregado  NO LLUVIOSO y su sueldo es su sueldo base mas adicional'"() {
         given:
         Restaurant restaurante = new Restaurant(nombre: 'La otra esquina').save(failOnError: true)
@@ -94,6 +135,49 @@ class RepartidorSpec extends Specification {
                 repartidor.liquidarSueldoFinal()
                 repartidor.sueldo.sueldoFinal == 30050
     }
+
+    
+        void "test repartidor tiene DOS pedidos entregados  NO LLUVIOSO y su sueldo es su sueldo base mas adicional"() {
+        given:
+        Restaurant restaurante = new Restaurant(nombre: 'La otra esquina').save(failOnError: true)
+        Repartidor repartidor = new  Repartidor("Juan", "9798797", restaurante).save(failOnError: true)
+        Ubicacion unaCasa = new Ubicacion(calle:'Av. Siempre viva', altura: 1234).save(failOnError: true)
+        Cliente cliente = new Cliente(nombre: 'Moni', apellido: 'Argento',  mail: 'moni.argento@gmail.com', ubicacion: unaCasa, telefono: '11-5555-4433')
+                .save()
+        Producto plato = new Plato(nombre: 'Alto Guiso', precio: 200, categoria: CategoriaPlato.PLATO, restaurant: restaurante)
+                .save(failOnError: true)
+        ModalidadEntrega modalidadEntrega = new ModalidadParaLlevar()
+                .save(failOnError: true)
+        EstadoPedido estado = new EstadoListo().save(failOnError: true)
+        Pedido pedido = new Pedido(cliente, modalidadEntrega, restaurante)
+        pedido.agregar(plato, 2)
+        pedido.estado = estado
+        pedido.save(failOnError: true)
+        pedido.siguienteEstado()
+        pedido.siguienteEstado()
+        pedido.save(failOnError: true)
+        modalidadEntrega.save(failOnError: true)
+        pedido.save(failOnError: true)
+
+        ModalidadEntrega modalidadEntrega2 = new ModalidadParaLlevar()
+                .save(failOnError: true)
+
+        Pedido pedido2 = new Pedido(cliente, modalidadEntrega2, restaurante)
+        pedido2.agregar(plato, 2)
+        pedido2.estado = estado
+        pedido2.save(failOnError: true)
+        pedido2.siguienteEstado()
+        pedido2.siguienteEstado()
+        pedido2.save(failOnError: true)
+
+        modalidadEntrega.save(failOnError: true)
+        
+        when:
+                pedido.save(failOnError: true)
+        then:
+                repartidor.liquidarSueldoFinal()
+                repartidor.sueldo.sueldoFinal == 30100
+                }
 
 
         void "test repartidor tiene un pedido entregado  LLUVIOSO y su sueldo es su sueldo base mas adicional'"() {
@@ -126,5 +210,100 @@ class RepartidorSpec extends Specification {
                 repartidor.liquidarSueldoFinal()
                 repartidor.sueldo.sueldoFinal == 30100 // 30000 sueldo base + 50 pedido completado + 50 adicional lluvia
     }
+
+
+        void "test repartidor tiene DOS pedidos entregados uno NO LLUVIOSO y otro LLUVIOSO y su sueldo es su sueldo base mas adicional"() {
+        given:
+        Restaurant restaurante = new Restaurant(nombre: 'La otra esquina').save(failOnError: true)
+        Repartidor repartidor = new  Repartidor("Juan", "9798797", restaurante).save(failOnError: true)
+        Ubicacion unaCasa = new Ubicacion(calle:'Av. Siempre viva', altura: 1234).save(failOnError: true)
+        Cliente cliente = new Cliente(nombre: 'Moni', apellido: 'Argento',  mail: 'moni.argento@gmail.com', ubicacion: unaCasa, telefono: '11-5555-4433')
+                .save()
+        Producto plato = new Plato(nombre: 'Alto Guiso', precio: 200, categoria: CategoriaPlato.PLATO, restaurant: restaurante)
+                .save(failOnError: true)
+        ModalidadEntrega modalidadEntrega = new ModalidadParaLlevar()
+                .save(failOnError: true)
+        EstadoPedido estado = new EstadoListo().save(failOnError: true)
+        Pedido pedido = new Pedido(cliente, modalidadEntrega, restaurante)
+        pedido.setClima(FabricaClima.crearPara("lluvioso"))
+
+        pedido.agregar(plato, 2)
+        pedido.estado = estado
+        pedido.save(failOnError: true)
+        pedido.siguienteEstado()
+        pedido.siguienteEstado()
+        pedido.save(failOnError: true)
+        modalidadEntrega.save(failOnError: true)
+        pedido.save(failOnError: true)
+
+        ModalidadEntrega modalidadEntrega2 = new ModalidadParaLlevar()
+                .save(failOnError: true)
+
+        Pedido pedido2 = new Pedido(cliente, modalidadEntrega2, restaurante)
+        pedido2.agregar(plato, 2)
+        pedido2.estado = estado
+        pedido2.save(failOnError: true)
+        pedido2.siguienteEstado()
+        pedido2.siguienteEstado()
+        pedido2.save(failOnError: true)
+
+        modalidadEntrega.save(failOnError: true)
+        
+        when:
+                pedido.save(failOnError: true)
+        then:
+                repartidor.liquidarSueldoFinal()
+                repartidor.sueldo.sueldoFinal == 30150
+                }
+
+
+        void "test repartidor tiene DOS pedidos entregados LLUVIOSOS y su sueldo es su sueldo base mas adicional"() {
+        given:
+        Restaurant restaurante = new Restaurant(nombre: 'La otra esquina').save(failOnError: true)
+        Repartidor repartidor = new  Repartidor("Juan", "9798797", restaurante).save(failOnError: true)
+        Ubicacion unaCasa = new Ubicacion(calle:'Av. Siempre viva', altura: 1234).save(failOnError: true)
+        Cliente cliente = new Cliente(nombre: 'Moni', apellido: 'Argento',  mail: 'moni.argento@gmail.com', ubicacion: unaCasa, telefono: '11-5555-4433')
+                .save()
+        Producto plato = new Plato(nombre: 'Alto Guiso', precio: 200, categoria: CategoriaPlato.PLATO, restaurant: restaurante)
+                .save(failOnError: true)
+        ModalidadEntrega modalidadEntrega = new ModalidadParaLlevar()
+                .save(failOnError: true)
+        EstadoPedido estado = new EstadoListo().save(failOnError: true)
+        Pedido pedido = new Pedido(cliente, modalidadEntrega, restaurante)
+        pedido.setClima(FabricaClima.crearPara("lluvioso"))
+
+        pedido.agregar(plato, 2)
+        pedido.estado = estado
+        pedido.save(failOnError: true)
+        pedido.siguienteEstado()
+        pedido.siguienteEstado()
+        pedido.save(failOnError: true)
+        modalidadEntrega.save(failOnError: true)
+        pedido.save(failOnError: true)
+
+        ModalidadEntrega modalidadEntrega2 = new ModalidadParaLlevar()
+                .save(failOnError: true)
+
+        Pedido pedido2 = new Pedido(cliente, modalidadEntrega2, restaurante)
+        pedido2.setClima(FabricaClima.crearPara("lluvioso"))
+
+        pedido2.agregar(plato, 2)
+        pedido2.estado = estado
+        pedido2.save(failOnError: true)
+        pedido2.siguienteEstado()
+        pedido2.siguienteEstado()
+        pedido2.save(failOnError: true)
+
+        modalidadEntrega.save(failOnError: true)
+        
+        when:
+                pedido.save(failOnError: true)
+        then:
+                repartidor.liquidarSueldoFinal()
+                repartidor.sueldo.sueldoFinal == 30200
+        }
+
+
+        
 
 }
